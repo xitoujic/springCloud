98555fb8fafdcb6de311795f392511a27a88650d
xitoujic <1033761115@qq.com>
2017-11-05 17:14:12 +0800

    2
    2

--- study001/microservice-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
+++ study001/microservice-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -2,6 +2,7 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
